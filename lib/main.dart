dc2241dd5a9df4cbc7bda31336f532d50e91c77e
Chelsea Farley <chelseafarley91@gmail.com>
2022-01-12 20:04:23 +1300

    shared_preferences demo

---
+++ lib/main.dart
@@ -0,0 +1,172 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class UserInfo {
+  final String _name;
+  final int _age;
+
+  UserInfo(this._name, this._age);
+
+  String toJson() {
+    return jsonEncode({
+      "name": _name,
+      "age": _age
+    });
+  }
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+  final TextEditingController _nameController = TextEditingController();
+  final TextEditingController _ageController = TextEditingController();
+
+  @override 
+  void initState() {
+    super.initState();
+    _loadPrefs();
+    _nameController.addListener(_updateUserInfo);
+    _ageController.addListener(_updateUserInfo);
+  }
+
+  @override
+  void dispose() {
+    _nameController.dispose();
+    _ageController.dispose();
+    super.dispose();
+  }
+
+  void _updateUserInfo() async {
+    final prefs = await SharedPreferences.getInstance();
+    final userInfo = UserInfo(_nameController.text, int.parse(_ageController.text));
+    prefs.setString("userInfo", userInfo.toJson());
+
+    setState(() {
+    });
+  }
+
+  void _loadPrefs() async {
+    final prefs = await SharedPreferences.getInstance();
+    int savedCounterValue = prefs.getInt("counter") ?? 0;
+    String userInfoJson = prefs.getString("userInfo") ?? UserInfo("", 0).toJson();
+
+    final userInfo = json.decode(userInfoJson);
+    setState(() {
+      _counter = savedCounterValue;
+      _nameController.text = userInfo["name"];
+      _ageController.text = userInfo["age"].toString();
+    });
+  }
+
+  void _incrementCounter() async {
+    final prefs = await SharedPreferences.getInstance();
+    prefs.setInt("counter", _counter + 1);
+
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text("Name: " + _nameController.text),
+            Text("Age: " + _ageController.text),
+            TextField(
+              controller: _nameController,
+              decoration: const InputDecoration(
+                border: UnderlineInputBorder(),
+                labelText: "Name"
+              ),
+            ),
+            const SizedBox(height: 16),
+            TextField(
+              controller: _ageController,
+              keyboardType: TextInputType.number,
+              decoration: const InputDecoration(
+                border: UnderlineInputBorder(),
+                labelText: "Age"
+              ),
+            ),
+            const SizedBox(height: 16),
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
